7a9fd35c9adc1256ac9faae323ac6561ea5eac6a
Naman Sharma <naman99sha@gmail.com>
2024-08-15 19:23:37 +0530

    updated token endpoint to also return email

--- app/main.py
+++ app/main.py
@@ -70,7 +70,7 @@ async def login_for_access_token(user: UserCreate):
     access_token = create_access_token(
         data={"sub": user["email"]}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer", "user_email":user.email}
 
 @app.get("/get-users")
 async def get_users():
